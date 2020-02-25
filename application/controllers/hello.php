3ee8a779087f2df66e8adcc40992ef8bf5b104e2
yudi iriyanto <52985439+riyan-web@users.noreply.github.com>
2020-02-25 23:57:50 +0700

    configurasi di CI

---
+++ application/controllers/hello.php
@@ -0,0 +1,12 @@
+<?php
+
+class Hello extends CI_Controller{
+
+	public function index () {
+		$this->load->view('contoh_view');
+	}
+
+	
+}
+
+?>
---
+++ application/views/contoh_view.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh View</title>
+</head>
+<body>
+    <h1> Contoh View Pada Codeigniter</h1>
+</body>
+</html>
