1b1b67113aff60fbedee5a1a4ede21e4bec96b0c
yudi iriyanto <52985439+riyan-web@users.noreply.github.com>
2020-02-26 10:39:00 +0700

    configurasi 2

---
+++ application/controllers/template/Overview.php
@@ -0,0 +1,14 @@
+<?php
+
+class Overview extends CI_Controller {
+    public function __construct()
+    {
+		parent::__construct();
+	}
+
+	public function index()
+	{
+        // load view template/overview.php
+        $this->load->view("template/overview");
+	}
+}
