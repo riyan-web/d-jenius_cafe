06777273ef8158a5648c3b8d57d4db9c6685da83
yudi iriyanto <52985439+riyan-web@users.noreply.github.com>
2020-02-27 12:44:02 +0700

    desain laporan keuangan

--- application/views/laporan/lap_keuangan.php
+++ application/views/laporan/lap_keuangan.php
@@ -2,60 +2,64 @@
         <h3>Laporan Keuangan</h3>
         <!-- <h6><b>Tambah Kategori</b></h6>
         <h6><b>Tambah Menu</b></h6> -->
-</div>
-    <div id="page-wrapper">
-          
+        </div>
+        <div id="page-wrapper">
+        <br>
           <div class="row">
                <!--page header-->
-              <div class="panel-body">
-                          <div class="row-6">
+              <div class="col-lg-12">
+                <div align="center">
+                  <a style="margin-bottom:10px" href="lap_keuangan.php" class="btn btn-danger btn-xs pull-left"><b><span class='fa fa-print'></span> Cetak PDF</b></a>
+                          <div class="row">
                               <div class="col-lg-12">
                                   <div class="panel-body">
                                       <div class="table-responsive">
-                                          <table style="background-color: yellow;" class="table table-bordered table-hover " id="tabel">
-                                          <a style="margin-bottom:10px" href="lap_keuangan.php" class="btn btn-danger btn-xs pull-right"><b><span class='fa fa-print'></span> Cetak PDF</b></a> 
+                                          <table style="background-color: transparant;" class="table table-bordered table-hover " id="tabel">
                                               <tr>
-                                                  <th>modal <a href="history_modal.php" class="pull-right btn btn-success btn-xs" title="semua data modal yang ditambahkan"><i class="fa fa-book"></i></a></th>
-                                              </tr>        
+                                                  <th><h3 style="color: blue;">Modal</h3><a href="history_modal.php" class="pull-right btn btn-success btn-xs" title="semua data modal yang ditambahkan"><i class="fa fa-book"></i></a></th>
+                                                  <td class="text-right"> <span class="text-left">
+                                                  <a href="tambah_modal.php" class="btn btn-primary btn-xs" title="tambah modal awal">Tambah Modal</a></span><h3 style="color: blue;">Rp.0000000</h3></td>
+                                              </tr>      
                                               <tr>
-                                                  <th style="color: blue;">Pengeluaran</th>
+                                                  <th><h3 style="color: blue;">Pengeluaran</h3></th>
                                                   <td class="text-right">20.000</td>
                                                   
                                               </tr>
                                             
                                               <tr>
-                                                  <th>Pemasukan</th>
+                                                  <th><h3 style="color: blue;">Pemasukan</h3></th>
                                                   <td class="text-right">Rp.</td>
                                                   
                                               
                                               <tr>
-                                                  <th>Piutang</th>
+                                                  <th><h3 style="color: blue;">Piutang</h3></th>
                                               </tr>
                                               
                                               <tr>
-                                                  <th>Hutang</th>
+                                                  <th><h3 style="color: blue;">Hutang</h3></th>
                                               </tr>
                                               
                                               <tr>
-                                                  <th>Operasional</th>
+                                                  <th><h3 style="color: blue;">Operasional</h3></th>
                                               </tr>
                                 
                                               <tr>
-                                                  <th>inventaris</th>
+                                                  <th><h3 style="color: blue;">Inventaris</h3></th>
                                               </tr>
                                               <tr>
                                         
-                                                  <th>Sisa Modal</th>
+                                                  <th><h3 style="color: blue;">Sisa Modal</h3></th>
                                               </tr>
 
                                           </table>
 
-                                      </div>
-                                  </div>   
-                              </div>
-                          </div>
-              </div>   
-          </div>
-      </div>
-    </div>
+                                          </div>
+                                    </div>   
+                                </div>
+                            </div>
+                        </div>
+                </div>   
+            </div>
+       
+     
       <!-- end page-wrapper -->
